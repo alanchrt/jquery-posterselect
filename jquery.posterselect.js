454aba9110cf0192fde9cdaae7645b78402e966b
Alan Christopher Thomas <alan.christopher.thomas@gmail.com>
2010-10-07 13:09:30 -0500

    added defaults

--- jquery.posterselect.js
+++ jquery.posterselect.js
@@ -4,6 +4,23 @@
     $.fn.extend({
 
         posterselect: function(options) {
+            // Apply posterselect to elements
+
+            // Set some reasonable defaults
+            var defaults = {
+                time: '2',
+                conversion_url: './?time=%s',
+                callback: function(image, time) {}
+            }
+
+            // Override the defaults with user settings
+            var options = $.extend(defaults, options);
+
+            // Apply plugin to each element
+            return this.each(function() {
+                var o = $.extend({}, options);
+                var obj = $(this);
+            });
 
         }
 

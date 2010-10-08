48d4376733ce52f7a0ba36a575f6b5cacd831fcb
Alan Christopher Thomas <alan.christopher.thomas@gmail.com>
2010-10-08 15:41:27 -0500

    cleaned up python lib

--- src/lib/posterselect.py
+++ src/lib/posterselect.py
@@ -18,15 +18,15 @@ class Posterselect():
         """Determines the filename of the video from its url."""
         return url.replace(self.video_url, '', 1)
 
-    def destination_filename(self, source_path, time):
-        """Determines the poster filename from the video path."""
-        source_timestamp = os.path.getmtime(source_path)
-        return '%s-%s.jpg' % (hashlib.sha1(
-            source_path + source_timestamp).hexdigest(), time)
-
     def source_path(self, source_filename):
         """Determines the video path from its filename."""
         return os.path.join(self.video_root, source_filename)
+
+    def destination_filename(self, source_path, time):
+        """Determines the poster filename from the video path."""
+        timestamp = os.path.getmtime(source_path)
+        return '%s-%s.jpg' % (hashlib.sha1(
+            source_path + str(timestamp)).hexdigest(), str(time))
 
     def destination_path(self, destination_filename):
         """Determines the poster image path from its filename."""
@@ -34,15 +34,16 @@ class Posterselect():
 
     def destination_url(self, destination_filename):
         """Determines the poster image url from its filename."""
-        return ''.join(self.poster_url, destination_filename)
+        return self.poster_url + destination_filename
 
     def run(self, source_path, destination_path, time):
         """Runs the ffmpeg command to extract the poster image."""
-        hms_time = datetime.timedelta(seconds=time).__str__()
+        hms_time = str(datetime.timedelta(seconds=time))
         if not os.path.exists(destination_path):
             cmd = ('ffmpeg', '-i', source_path, '-vframes', '1', '-ss',
                    hms_time, '-f', 'image2', '-vcodec', 'mjpeg',
                    destination_path)
+            print ' '.join(cmd)
             subprocess.Popen(cmd)
 
     def extract(self, url, time):
@@ -60,8 +61,7 @@ class Posterselect():
         plugin.
         """
         destination_url = self.extract(url, time)
-        return """
-        {
+        return """{
             posterselect: {
                 video: {
                     url: %(source_url)s,
@@ -71,8 +71,7 @@ class Posterselect():
                     url: %(destination_url)s
                 }
             }
-        }
-        """ % {
+        }""" % {
             'source_url': url,
             'time': time,
             'destination_url': destination_url,
